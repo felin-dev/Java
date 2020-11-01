06971b2360a7492a6f0d67f546022fa98d3aed84
felin-dev <simeon.stanislavov.balabanov@gmail.com>
2020-11-01 16:45:15 +0200

    Added Problem Card Suit

---
+++ JavaOOP/01.ExercisesWorkingWithAbstraction/src/_1_CardSuit/Main.java
@@ -0,0 +1,11 @@
+package _1_CardSuit;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Card Ranks:");
+        Arrays.stream(Suit.values()).forEach(e ->
+                System.out.printf("Ordinal value: %d; Name value: %s%n", e.ordinal(), e));
+    }
+}
---
+++ JavaOOP/01.ExercisesWorkingWithAbstraction/src/_1_CardSuit/Suit.java
@@ -0,0 +1,5 @@
+package _1_CardSuit;
+
+public enum Suit {
+    ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+}
