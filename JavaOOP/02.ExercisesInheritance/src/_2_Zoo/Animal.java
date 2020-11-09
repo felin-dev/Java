c828c4c7818e0fb8b90b1743d3c216c6144099e3
felin-dev <simeon.stanislavov.balabanov@gmail.com>
2020-11-09 22:23:40 +0200

    Added Problem Zoo

---
+++ JavaOOP/02.ExercisesInheritance/src/_2_Zoo/Animal.java
@@ -0,0 +1,13 @@
+package _2_Zoo;
+
+public class Animal {
+    private final String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ JavaOOP/02.ExercisesInheritance/src/_2_Zoo/Bear.java
@@ -0,0 +1,7 @@
+package _2_Zoo;
+
+public class Bear extends Mammal {
+    public Bear(String name) {
+        super(name);
+    }
+}
