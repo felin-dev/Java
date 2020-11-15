28965fb0fb4db67861cd8da00d4a6f935122bba0
felin-dev <simeon.stanislavov.balabanov@gmail.com>
2020-11-15 22:11:08 +0200

    Added Problem Say Hello

---
+++ JavaOOP/04.LabInterfacesAndAbstraction/src/_3_SayHello/European.java
@@ -0,0 +1,20 @@
+package _3_SayHello;
+
+public class European implements Person {
+
+    private String name;
+
+    public European(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String sayHello() {
+        return "Hello";
+    }
+}
---
+++ JavaOOP/04.LabInterfacesAndAbstraction/src/_3_SayHello/Person.java
@@ -0,0 +1,8 @@
+package _3_SayHello;
+
+public interface Person {
+
+    String getName();
+
+    String sayHello();
+}
