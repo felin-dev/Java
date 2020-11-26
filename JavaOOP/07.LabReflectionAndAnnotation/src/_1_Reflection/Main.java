b2537e607fae05200f1c46146c83395dbf4a7f5c
felin-dev <simeon.stanislavov.balabanov@gmail.com>
2020-11-26 22:20:26 +0200

    Added Problem Reflection

---
+++ JavaOOP/07.LabReflectionAndAnnotation/src/_1_Reflection/Main.java
@@ -0,0 +1,20 @@
+package _1_Reflection;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class Main {
+    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        Class<Reflection> reflectionClass = Reflection.class;
+
+        System.out.println(reflectionClass);
+        System.out.println(reflectionClass.getSuperclass());
+        Class<?>[] interfaces = reflectionClass.getInterfaces();
+
+        for (Class<?> anInterface : interfaces) {
+            System.out.println(anInterface);
+        }
+
+        Reflection reflectionObject = reflectionClass.getDeclaredConstructor().newInstance();
+        System.out.println(reflectionObject);
+    }
+}
